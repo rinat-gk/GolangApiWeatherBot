d96d6e3083deab316cf806a8254425a10de8841a
rinat-gk <53332783+rinat-gk@users.noreply.github.com>
2024-10-17 18:42:43 +0400

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-const OpenWeatherMapAPIKey = "0e264e7da407afac79c77c3f2d1feeeb"
+const OpenWeatherMapAPIKey = "TokenID"
 
 // Creating a struct
 type WeatherMapApiResponse struct {
